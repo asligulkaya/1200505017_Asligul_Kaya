3d381cae29dc6e502a2828517c549969737f4815
asligulkaya <asligul.kaya01@gmail.com>
2023-05-15 22:47:48 +0300

    Algoritma Analizi ve Tasarımı Kısa Sınav-2 Kısım-1
    Aslıgül Kaya
    1200505017

---
+++ AsligulKaya1200505017.cpp
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+#define A_SIZE 20
+#define G_SIZE 10
+#define MAX_W 10
+
+//Max deger icin sonsuz sayi
+#define INF INT_MAX
+
+void generate(int a[], int size);
+void function1(int a[], int size);
+int function2(int a[], int size);
+void function3(int g[][G_SIZE], int d[][G_SIZE], int size);
+void print1(int a[], int size);
+void print2(int g[][G_SIZE], int size);
+void print3(int g[][G_SIZE], int size, int t);
+
+int main() {
+    int a[A_SIZE];
+    int g[G_SIZE][G_SIZE] = {
+    { 0, 10,  3,  0,  0,  5,  0, 17,  0, 22},
+    {10,  0,  5,  0,  2,  0, 13,  0,  0,  0},
+    { 3,  5,  0,  2,  0,  4,  0, 21,  0, 11},
+    { 0,  0,  2,  0,  7,  0,  6,  0,  0,  0},
+    { 0,  2,  0,  7,  0,  6,  0,  0, 19,  0},
+    { 5,  0,  4,  0,  6,  0,  9,  3,  0,  0},
+    { 0, 13,  0,  6,  0,  9,  0,  4,  0,  0},
+    {17,  0, 21,  0,  0,  3,  4,  0,  8,  0},
+    { 0,  0,  0,  0, 19,  0,  0,  8,  0,  5},
+    {22,  0, 11,  0,  0,  0,  0,  0,  5,  0}
+    };
+    int d[G_SIZE][G_SIZE];
+    int t;
+
+    // Rastgele sayi uretici icin seed
+    srand(time(NULL));
+
+    // Calisma suresinin hesaplanmasi
+    clock_t start, end;
+    double time;
+
+    start = clock();
+
+    generate(a, A_SIZE);
+    printf("A:\n");
+    print1(a, A_SIZE);
+
+    function1(a, A_SIZE);
+    printf("\nFunction1 Sonuc:\n");
+    print1(a, A_SIZE);
+
+    t = function2(a, A_SIZE);
+    printf("\nFunction2 Sonuc: %d\n", t);
+
+    printf("\nG:\n");
+    print2(g, G_SIZE);
+
+    function3(g, d, G_SIZE);
+    printf("\nFunction3 Sonuc:\n");
+    print2(d, G_SIZE);
+
+    printf("\n%d icin sonuc:\n", t);
+    print3(d, G_SIZE, t);
+
+    end = clock();
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\nCalisma Suresi: %f sn\n", time);
+
+    return 0;
+}
+
+void generate(int a[], int size) {
+    for (int i = 0; i < size; i++) {
+        a[i] = rand() % (2 * MAX_W) - MAX_W;
+    }
+}
+
+void quickSort(int a[], int left, int right) {
+    int i = left, j = right;
+    int pivot = a[(left + right) / 2];
+
+    while (i <= j) {
+        while (a[i] < pivot) {
+            i++;
+        }
+        while (a[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j) {
+        quickSort(a, left, j);
+    }
+    if (i < right) {
+        quickSort(a, i, right);
+    }
+}
+
+void function1(int a[], int size) {
+    quickSort(a, 0, size - 1);
+}
+
+int function2(int a[], int size) {
+    int t = 0, current_sum = 0, count=0;
+    for (int i = 0; i < size; i++) {
+        if (current_sum + a[i] > 0) {
+            current_sum = current_sum + a[i];
+        }
+        else {
+            current_sum = 0;
+        }
+        if (current_sum > t) {
+            t = current_sum;
+            count++;
+        }
+    }
+    return t/count;
+}
+
+void function3(int g[][G_SIZE], int d[][G_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        int visited[size];
+        int dist[size];
+
+        for (int j = 0; j < size; j++) {
+            visited[j] = 0;
+            dist[j] = INF;
+        }
+
+        dist[i] = 0;
+
+        for (int k = 0; k < size; k++) {
+            int min_dist = INF;
+            int u;
+
+            for (int j = 0; j < size; j++) {
+                if (!visited[j] && dist[j] <= min_dist) {
+                    min_dist = dist[j];
+                    u = j;
+                }
+            }
+
+            visited[u] = 1;
+
+            for (int v = 0; v < size; v++) {
+                if (g[u][v] != 0 && !visited[v]) {
+                    int alt_dist = dist[u] + g[u][v];
+                    if (alt_dist < dist[v]) {
+                        dist[v] = alt_dist;
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < size; j++) {
+            d[i][j] = dist[j];
+        }
+    }
+}
+
+void print1(int a[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", a[i]);
+        if ((i + 1) % 10 == 0) {
+            printf("\n");
+        }
+    }
+}
+
+void print2(int g[][G_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (g[i][j] == INF) {
+                printf("INF ");
+            }
+            else {
+                printf("%3d ", g[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void print3(int d[][G_SIZE], int size, int t) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i != j && d[i][j] < t) {
+                printf("%c -> %c: %d\n", 'A' + i, 'A' + j, d[i][j]);
+            }
+        }
+    }
+}
